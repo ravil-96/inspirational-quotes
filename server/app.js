65748e24f8b61fb4bfa1cf89bb845577bd80efd5
Ravil <ravil-96@mail.ru>
2021-03-09 11:17:49 +0000

    tweaking express server and fixing quotes object

--- server/app.js
+++ server/app.js
@@ -1,35 +1,41 @@
-//const express = require('express')
-//const app = express()
-//const cors = require('cors');
-const http = require ('http');
-const requestListener = (request,response) => {
-  response.statusCode = 200;
-  response.end('Hello World!');
-};
+const express = require('express');
+//const http = require('http');
+const server = express();
 
+const cors = require('cors');
+server.use(cors());
+
+const port = 3000
 const host = 'localhost'
-const port = 8000
 
-const server = http.createServer(requestListener);
-server.listen(port, host, ()=> console.log('I am here!'))
+//const requestListener = function (req, res) {
+  //res.setHeader("Content-Type", "text/html");
+  //res.writeHead(200);
+  //res.end(`<html><body><h1>This is HTML</h1></body></html>`);
+//};
 
-quotes = [
-  '"PERFECT IS THE ENEMY OF GOOD." –VOLTAIRE',
-  '"I’M STILL LEARNING." –MICHELANGELO',
-  '"LIFE IS A JOURNEY, NOT A DESTINATION." –RALPH WALDO EMERSON',
-  '"LEARNING IS NOT ATTAINED BY CHANCE, IT MUST BE SOUGHT FOR WITH ARDOR AND ATTENDED TO WITH DILIGENCE." ―ABIGAIL ADAMS',
-  '"YESTERDAY I WAS CLEVER, SO I CHANGED THE WORLD. TODAY I AM WISE, SO I AM CHANGING MYSELF." –RUMI',
-  '"BE CURIOUS, NOT JUDGMENTAL." –WALT WHITMAN',
-  '"YOU DON’T HAVE TO BE GREAT TO START, BUT YOU HAVE TO START TO BE GREAT." –ZIG ZIGLAR',
-  '"BE STUBBORN ABOUT YOUR GOALS AND FLEXIBLE ABOUT YOUR METHODS." –UNKNOWN',
-  '"NOTHING WILL WORK UNLESS YOU DO." –MAYA ANGELOU',
-  '"NEVER GIVE UP ON A DREAM JUST BECAUSE OF THE TIME IT WILL TAKE TO ACCOMPLISH IT. THE TIME WILL PASS ANYWAY." –EARL NIGHTINGALE',
-  '"ANYONE WHO STOPS LEARNING IS OLD, WHETHER AT TWENTY OR EIGHTY." —HENRY FORD',
-  '"TELL ME AND I FORGET. TEACH ME AND I REMEMBER. INVOLVE ME AND I LEARN." –BENJAMIN FRANKLIN',
-  '"CHANGE IS THE END RESULT OF ALL TRUE LEARNING." ―LEO BUSCAGLIA',
-  '"LIVE AS IF YOU WERE TO DIE TOMORROW. LEARN AS IF YOU WERE TO LIVE FOREVER." ―MAHATMA GANDHI',
-  '"A LEARNING CURVE IS ESSENTIAL TO GROWTH." –TAMMY BJELLAND'
-]
+//const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`I am here! Find me running on http://${host}:${port}`);
+});
+
+const quotes = {
+  quote1:'"PERFECT IS THE ENEMY OF GOOD." –VOLTAIRE',
+  quote2:'"I’M STILL LEARNING." –MICHELANGELO',
+  quote3:'"LIFE IS A JOURNEY, NOT A DESTINATION." –RALPH WALDO EMERSON',
+  quote4:'"LEARNING IS NOT ATTAINED BY CHANCE, IT MUST BE SOUGHT FOR WITH ARDOR AND ATTENDED TO WITH DILIGENCE." ―ABIGAIL ADAMS',
+  quote5:'"YESTERDAY I WAS CLEVER, SO I CHANGED THE WORLD. TODAY I AM WISE, SO I AM CHANGING MYSELF." –RUMI',
+  quote6:'"BE CURIOUS, NOT JUDGMENTAL." –WALT WHITMAN',
+  quote7:'"YOU DON’T HAVE TO BE GREAT TO START, BUT YOU HAVE TO START TO BE GREAT." –ZIG ZIGLAR',
+  quote8:'"BE STUBBORN ABOUT YOUR GOALS AND FLEXIBLE ABOUT YOUR METHODS." –UNKNOWN',
+  quote9:'"NOTHING WILL WORK UNLESS YOU DO." –MAYA ANGELOU',
+  quote10:'"NEVER GIVE UP ON A DREAM JUST BECAUSE OF THE TIME IT WILL TAKE TO ACCOMPLISH IT. THE TIME WILL PASS ANYWAY." –EARL NIGHTINGALE',
+  quote11:'"ANYONE WHO STOPS LEARNING IS OLD, WHETHER AT TWENTY OR EIGHTY." —HENRY FORD',
+  quote12:'"TELL ME AND I FORGET. TEACH ME AND I REMEMBER. INVOLVE ME AND I LEARN." –BENJAMIN FRANKLIN',
+  quote13:'"CHANGE IS THE END RESULT OF ALL TRUE LEARNING." ―LEO BUSCAGLIA',
+  quote14:'"LIVE AS IF YOU WERE TO DIE TOMORROW. LEARN AS IF YOU WERE TO LIVE FOREVER." ―MAHATMA GANDHI',
+  quote15:'"A LEARNING CURVE IS ESSENTIAL TO GROWTH." –TAMMY BJELLAND'
+}
 
 function getRandomQuote () {
   return quotes[4]
--- server/html.js
+++ server/html.js
@@ -1,9 +1,20 @@
-const http = require ('http');
-
+const http = require('fs').promises;
 const host = 'localhost'
 const port = 8000
 
-const requestListener = (request,response) => {};
+const requestListener = (req, res) => {
+    fs.readFile(__dirname + "/index.html")
+       .then(contents => {
+           res.setHeader("Content-Type", "text/html");
+           res.writeHead(200);
+           res.end(contents);
+       })
+       .catch(err => {
+           res.writeHead(500);
+           res.end(err);
+           return;
+       });
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
