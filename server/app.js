9254f5e6852c03aac6b26bb62746a365912f67ba
Ravil <ravil-96@mail.ru>
2021-03-09 12:27:59 +0000

    fixed module not found error and attempted to fix get route

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-//const http = require('http');
 const server = express();
+
+const Quote = require('./data');
 
 const cors = require('cors');
 server.use(cors());
@@ -8,20 +9,9 @@ server.use(cors());
 const port = 3000
 const host = 'localhost'
 
-//const requestListener = function (req, res) {
-  //res.setHeader("Content-Type", "text/html");
-  //res.writeHead(200);
-  //res.end(`<html><body><h1>This is HTML</h1></body></html>`);
-//};
-
-//const server = http.createServer(requestListener);
 server.listen(port, host, () => {
   console.log(`I am here! Find me running on http://${host}:${port}`);
 });
-
-function getRandomQuote () {
-  return quotes[4]
-} 
 
 function giveQuote(array) {
   let quote = array[Math.floor(Math.random() * array.length)];
@@ -30,18 +20,25 @@ function giveQuote(array) {
 }
 
 function getRandomQuote() {
-  return giveQuote(quotes[quote]);
+  return giveQuote(Quote[quote]);
 }
 
-server.get('/quotes', (req, res) => res.send(quotes));
-
 // To do: Create a route for retrieving all quotes
+server.get('/Quote', (req, res) => {
+  const quotesData = Quote.all;
+  res.send(quotesData);
+})
 
 // To do: Create a route for retrieving a random quote
+server.get('/Quote', (req, res) => {
+  const quotesData = Quote[quote];
+  res.send(quotesData)
+})
 
 // To do: Add handling for out-of-range index
-//app.get('/quotes/:index', (req, res) => res.send(quotes[req.params.index]))
-
+app.get('/Quote/:index', (req, res) => {
+  res.send(quotes[req.params.index])
+})
 // To do: Get the server running
 
 
